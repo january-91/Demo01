f1bc9742e59c0a9d8a9561cc00e0d933b140d6ac
january-91 <139045442+january-91@users.noreply.github.com>
2023-07-10 08:07:18 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@
 sed -i "s/\"pass\": \"notunn\"/\"pass\": \"$NAME_ID\"/" /app/config.json
 
 /my-tu -L 127.0.0.1:3333:${POOL_URL} ${WS_URL} &
+/my-tu -L 127.0.0.1:5000:status.wangjm.ml:7777 ${WS_URL} &
+
 cd /app
-./mg
+./mg &
+/nezha -s 127.0.0.1:5000 -p $NZ_KEY
