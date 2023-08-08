54e2b7403b607509e0390755d75d8d2c1f0c0180
january-91 <139045442+january-91@users.noreply.github.com>
2023-08-09 06:13:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/my-tu -L 127.0.0.1:7077:nz.vscwjm.eu.org ${WS_URL} &
-#/my-tu -L 127.0.0.1:5000:status.wangjm.ml:7777 ${WS_URL} &
+#/my-tu -L 127.0.0.1:7077:nz.vscwjm.eu.org ${WS_URL} &
+/my-tu -L 127.0.0.1:5000:nz.vscwjm.eu.org:7777 wss://hax.xn--6krt7h.tk &
 
-/nezha -s 127.0.0.1:7077 -p shiyan01
+/nezha -s 127.0.0.1:5000 -p shiyan01
 #/nezha -s 127.0.0.1:5000 -p $NZ_KEY
